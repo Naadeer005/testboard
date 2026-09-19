7daa692876eae7142b2bcc41fb1868e4ca4a378b
Naadeer005 <nadeejomjom@gmail.com>
2026-09-19 13:54:23 +0700

    2

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,28 @@ void loop() {
     }
     if (input.length() == 1)
     {
-      
+      digitalWrite(CLK, '1');
+      delay(250);
+      digitalWrite(CLK, '0');
+    }
+    if (input.length() == 4) {
+
+      digitalWrite(CS, input[0] == '1' ? HIGH : LOW);
+      digitalWrite(SCLK, input[1] == '1' ? HIGH : LOW);
+      digitalWrite(MOSI, input[2] == '1' ? HIGH : LOW);
+
+      Serial.print("CS = ");
+      Serial.println(input[0]);
+
+      Serial.print("SCLK = ");
+      Serial.println(input[1]);
+
+      Serial.print("MOSI = ");
+      Serial.println(input[2]);
+      digitalWrite(CLK, '1');
+      delay(250);
+      digitalWrite(CLK, '0');
+
     }
      else {
       Serial.println("Please enter 1,3,4 bits only.");
